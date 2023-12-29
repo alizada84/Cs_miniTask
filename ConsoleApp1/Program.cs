27aa9af82e2b0b6df44529b87a76d50457aa2874
Ibrahim <alizadaibrahimm07@gmail.com>
2023-12-29 12:54:06 +0400

    Cs mini Task

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Task 1
+
+//string result = null;
+
+//Console.WriteLine("Please Enter string value and click enter: ");
+//for (int i = 0; i < 3; i++)
+//{
+//    string? value = Console.ReadLine();
+//    result += value;
+//}
+
+//Console.WriteLine(result);
+
+
+// Task 2
+
+//Console.Write("Enter your name: ");
+//string name = Console.ReadLine();
+
+//Console.Write("Enter your surname: ");
+//string surname = Console.ReadLine();
+
+//Console.WriteLine($"Name: {name}, Surname: {surname}");
+
+
+// Task 3
+
+//Console.WriteLine("--- Beraber Yanli Duzbucaq ---");
+
+//Console.Write("En Daxil edin: ");
+//uint en = uint.Parse(Console.ReadLine());
+
+//Console.Write("Uzunluq daxil edin: ");
+//uint uzunluq = uint.Parse(Console.ReadLine());
+
+//uint perimetr = 2 * (en + uzunluq);
+//uint sahe = en * uzunluq;
+
+//Console.WriteLine($"PERIMETR: {perimetr}, SAHE: {sahe}");
